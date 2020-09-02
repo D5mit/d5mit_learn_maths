faef99ccb294d186e5a25f08b935d24b62b26fff
danie <dsmit2@gmail.com>
2020-09-02 09:39:07 +0200

    Initial upload

--- d5mit_learn_maths/main.py
+++ d5mit_learn_maths/main.py
@@ -2,15 +2,13 @@ class Maths_Test:
     """ Maths Test class.  This class is the actual math test that is being presented to a person to write.
 
     Parameters:
-    argument1 (int): Description of arg1
-
     description (string): Description of the test that is being written
     start_time  #toDo
     end_Time    #toDo
     questions (DF)
-        Question
-        Answer
-        How many times to ask the question
+        question (str) -  The question
+        answer (str) - The correct answer of the question
+        nrQuestions -  How many times to ask the question
         How many times question answered correctly    #toDo
         How many times question answered incorrectly  #toDo
     person    #toDo
@@ -20,9 +18,31 @@ class Maths_Test:
         self.description = description
         self.questions = questions
 
+    def get_test(self):
+        return self.description
+
     def set_questions(self, questions):
         self.questions = questions
 
     def get_questions(self):
         return self.questions
 
+    def ask_question(self):
+        import numpy as np
+
+        # test_questions = self.questions
+        test_questions = self.questions
+
+        # get the total number of questions, note the program can ask the same question more than once
+        nrOfQuest = np.sum(test_questions['nrQuestions'])
+
+        # calculate percentage of each question, this is something like [0.5 0.5]
+        questLeft_ratio_array = test_questions['nrQuestions'] / nrOfQuest
+
+        # choose one question randomly and assign a higher prob to this question
+        questLeft_ratio_multi_array = np.random.multinomial(1, list(questLeft_ratio_array))
+
+        # choose one question to ask
+        questionIndex = np.argmax(questLeft_ratio_multi_array)
+
+        return list(test_questions.iloc[questionIndex])
--- d5mit_learn_maths/test_main.py
+++ d5mit_learn_maths/test_main.py
@@ -1,20 +1,39 @@
 from .main import Maths_Test
+import pandas as pd
 
+# create object
 math_test = Maths_Test('Testing')
 
 
-def test_set_questions_1():
-    math_test.set_questions([22, 26])
-    assert (math_test.get_questions() == [22, 26])
+def test_set_questions_0():
+    maths_test = Maths_Test('Some basic questions')
+    assert (maths_test.get_test() == 'Some basic questions')
 
 
-# def test_days_until_launch_0():
-#     assert (days_until_launch(253, 253) == 0)
-#
-#
-# def test_days_until_launch_0_negative():
-#     assert (days_until_launch(83, 64) == 0)
-#
-#
-# def test_days_until_launch_1():
-#     assert (days_until_launch(9, 10) == 1)
+def test_get_questions_1():
+    maths_test = Maths_Test('Some basic questions')
+
+    # create dataframe with the questions
+    questions = {'Question': ['What is 1 + 1?', 'What is 1 + 2?'],
+                 'Answer': ['2', '3']}
+
+    test_questions = pd.DataFrame(questions)
+
+    # set questions
+    maths_test.set_questions(test_questions)
+    pd.testing.assert_frame_equal(maths_test.get_questions(), test_questions)
+
+
+def test_ask_question_1():
+
+    # create dataframe with the questions
+    questions = {'Question': ['1 + 1 =', '1 + 2 ='],
+                 'Answer': ['2', '3'],
+                 'nrQuestions': [0, 3]}
+
+    # convert to dataframe
+    test_questions = pd.DataFrame(questions)
+
+    maths_test = Maths_Test('Some basic questions', test_questions)
+
+    assert (maths_test.ask_question() == ['1 + 2 =', '3', 3])
